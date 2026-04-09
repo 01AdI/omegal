788ea929fd86b19ac347b2dbad8c9b2eb8afedc0
Aditya Rathore <145015316+01AdI@users.noreply.github.com>
2026-04-09 22:25:09 +0530

    Update client-socket.js

--- client-socket.js
+++ client-socket.js
@@ -1,10 +1,8 @@
 import { io } from "socket.io-client";
 
 const client_socket = io(window.location.origin, {
-  auth: {
-    token: localStorage.getItem("token"),
-  },
-  transports: ["websocket"],
+  autoConnect: false, // don't connect until we explicitly call .connect()
+  transports: ["polling", "websocket"], // polling first, upgrades to WS
 });
 
 export default client_socket;
